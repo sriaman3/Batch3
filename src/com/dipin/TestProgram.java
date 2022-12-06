28a6537bf9006e6d4ff5d1581cfae831e8380b9e
sriam <sriaman3@gmail.com>
2022-12-06 06:58:13 +0530

    test

---
+++ src/com/dipin/TestProgram.java
@@ -0,0 +1,14 @@
+package com.dipin;
+
+public class TestProgram {
+
+	public static void main(String[] args) {
+		
+		System.out.print("My name is dipin");
+		System.out.println("My name is Bishnu");
+		System.out.println("My name is Bishn");
+		System.out.println("My name is Bishn");
+
+	}
+
+}
